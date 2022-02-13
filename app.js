d0f1cecfd57c3ff80c88d4b48f716f4ff5aa6f1e
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-13 23:52:27 +0300

    create course

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/education').then(() => {
+  console.log('DB Succsess');
+});
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 
