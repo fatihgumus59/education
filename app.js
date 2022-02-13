8320fa98ee5f4b19eef3a436c29aebcea1a88d20
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-13 23:04:51 +0300

    ejs template

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get('/',async(req,res)=>{
+app.set("view engine", "ejs");
 
-    res.status(200).send('Hello!');
-})
+app.use(express.static("public"));
 
+app.get("/", async (req, res) => {
+  res.status(200).render("index", {
+    navigation_active: "index",
+  });
+});
 
-
+app.get("/about", async (req, res) => {
+  res.status(200).render("about", {
+    navigation_active: "about",
+  });
+});
 
 const port = 3000;
 
-app.listen(port,()=>{
-
-    console.log(`Sunucu ${port} portunda başlatıldı.`);
-
-})
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
