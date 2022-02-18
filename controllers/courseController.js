aac4bf867181cf022b127d04933df529fd04b458
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-18 23:52:02 +0300

    course page category added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -46,10 +46,24 @@ exports.getAllCourse = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
+    const categorySlug = req.query.categories;
+    const category = await Category.findOne({ slug: categorySlug });
+
+    let filter = {};
+
+    if (categorySlug) {
+      filter = { category: category._id };
+    }
+
+    const courses = await Course.find(filter).sort('-createdAt');
+    const categories = await Category.find();
+    
     const course = await Course.findOne({ slug: req.params.slug });
 
     res.status(200).render('course', {
       course,
+      categories,
+      courses,
       navigation_active: 'courses',
     });
   } catch (error) {
