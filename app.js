d284cdd13f4d8cd94b0e1f73866ae711cc7d5e68
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-23 21:52:58 +0300

    register form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
+const UserRoute = require('./routes/UserRoute');
+
 
 const app = express();
 
@@ -20,6 +22,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
+app.use('/users', UserRoute);
 
 
 const port = 3000;
