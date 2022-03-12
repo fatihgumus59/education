b3e2003a8b8995ac11562f8d107c829bbf42ac7d
fatihgumus59 <fatihgumus72@gmail.com>
2022-03-12 15:40:41 +0300

    contact page activated

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = async (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -23,3 +25,50 @@ exports.getLoginPage = async (req, res) => {
   });
 };
 
+exports.getContactPage = async (req, res) => {
+  res.status(200).render('contact', {
+    navigation_active: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+  const templateMessage = `
+
+  <h3>Message Details</h3><br>
+  <h3>Subject: ${req.body.subject}</h3>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <p>${req.body.message}</p>
+  
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'fatihgumus72@gmail.com', // generated gmail user
+      pass: 'brsabmkwhjyvrbdk', // generated gmail password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Education Contact Form: " <fatihgumus72@gmail.com>', // sender address
+    to: 'fatihgumus72@gmail.com', // list of receivers
+    subject: req.body.subject, // Subject line
+    html: templateMessage, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect('/contact');
+};
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
-router.route('/login').get(redirectMiddleware, pageController.getLoginPage);
-router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
+router.route('/login').get(redirectMiddleware, pageController.getLoginPage); // giriş yapılı olduğu halde giriyorsa ana sayfaya gönder işlemleri
+router.route('/register').get(redirectMiddleware, pageController.getRegisterPage); // giriş yapılı olduğu halde giriyorsa ana sayfaya gönder işlemleri
+router.route('/contact').get( pageController.getContactPage);
+router.route('/contact').post( pageController.sendEmail);
 
 module.exports = router;
